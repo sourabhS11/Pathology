0ae9bb4ffdc0841f2e98f80c0742d9cc04977bc7
sourabhS11 <sourabhshinde2812@gmail.com>
2022-06-15 16:27:48 +0530

    added login and register page UI

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -38,6 +38,11 @@ namespace Pathology.Controllers
             return View();
         }
 
+        public IActionResult Register()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ Models/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pathology.Models
+{
+    public class Employee
+    {
+        public string fName { get; set; }
+        public string lName { get; set; }
+        public string email { get; set; }
+        public string addrs { get; set; }
+        public string qualification { get; set; }
+        public string role { get; set; }
+        public int pwd { get; set; }
+        public int adharId { get; set; }
+
+        [Display(Name = "Date of joining")]
+        [DataType(DataType.Date)]
+        public DateTime joinDate { get; set; }
+
+    }
+}
